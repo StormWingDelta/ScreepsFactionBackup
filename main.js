007a35c1a1799608e29a6ee9216f6d757b323c98
StormWingDelta <chrisjrogers11@gmail.com>
2019-01-30 17:10:40 -0800

    added builder and limiter code

--- main.js
+++ main.js
@@ -27,18 +27,25 @@ module.exports.loop = function () {
         if (creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
         }
-	if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
+		if (creep.memory.role == 'builder') {
+            roleUpgrader.run(creep);
         }
+		
     }
     //console.log('Orders Given!');
 
     // goal: have 10 harvesters and as many upgraders as possible
-    var minimumNumberOfHarvesters = 10;
+    var minimumNumberOfHarvesters = 5;
+	var minimumNumberOfUpgraders = 5;
+	var minimumNumberOfBuilders = 2;
 	
     // _.sum will count the number of properties in Game.creeps filtered by the
     //  arrow function, which checks for the creep being a harvester
     var numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester');
+	var numberOfUpgraders = _.sum(Game.creeps, (c) => c.memory.role == 'upgrader');
+	var numberOfBuilders = _.sum(Game.creeps, (c) => c.memory.role == 'builder');
+	
+	
     var creepbot = undefined;
 
     // if not enough harvesters
@@ -47,14 +54,16 @@ module.exports.loop = function () {
         //name = Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,MOVE], undefined,{ role: 'harvester', working: false});
         creepbot = Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,CARRY,MOVE,MOVE], 'Harv' + Game.time, {memory: {role: 'harvester', working: false}}); 
     }
+	else if(numberOfUpgraders < minimumNumberOfUpgraders){
+		creepbot = Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,CARRY,MOVE,MOVE], 'Upg' + Game.time, {memory: {role: 'upgrader', working: false}});
+		
+	}
+	else if(numberOfBuilders < minimumNumberOfBuilders){
+		creepbot = Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,CARRY,MOVE,MOVE], 'Build' + Game.time, {memory: {role: 'builder', working: false}});
+	}
     else {
-        // else try to spawn an upgrader
-        // small change from what you saw in the video: for upgraders it makes
-        //  more sense to have two move parts because they have to travel further
         //name = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined,{ role: 'upgrader', working: false});
-        creepbot = Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,CARRY,MOVE,MOVE], 'Upg' + Game.time, {memory: {role: 'upgrader', working: false}});
-
-
+        console.log("Spawn Limit Reached!!!!!!");
     }
 
     // print name to console if spawning was a success
